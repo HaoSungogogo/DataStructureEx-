fb235b8c79f57c451a090a8124d7a38b0f2dc239
HaoSungogogo <haosun2017@u.northwestern.edu>
2017-05-05 18:01:59 -0500

    stack and queue

---
+++ Queue and Stack/Queue and Deque.java
@@ -0,0 +1,15 @@
+Deque could be implemented by LinkedList and ArrayDequeue
+We just use LinkedList to implement Queue and Deque interface.
+ArrayDequeue: no null value in Deque
+
+using deque as stack
+
+Deque<Integer> stack = new LinkedList<>();
+stack.offerFrist();
+stack.pollFirst();
+stack.peekFirst();
+
+Queue<Integer> qu = new LinkedList<>();
+qu.offer();
+qu.poll();
+qu.peek();
