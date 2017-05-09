eefde7a7898eea995333da90975879a6cf5049f6
HaoSungogogo <haosun2017@u.northwestern.edu>
2017-05-09 11:59:44 -0500

    map complete

--- Map:Set:Hash/Set:Map:Hash.java
+++ Map:Set:Hash/Set:Map:Hash.java
@@ -16,3 +16,18 @@ Collection<V> values() get collection view of all the values in the HashMap
 * HashTable doesn not allow null key, HashMap allows
 * HashTable is synchronized and performance is poor.
 * HashMap can only have one null Key, it is always mapped to bucket 0.
+
+Very Important:
+key.hashCode() -> to determine the entry index of the key
+key.equals() -> to determine whether two keys are the same keys
+Defined in Object class.
+The default implementation of equals() is to check if two references are pointed to the same object.
+The default implementation of hashCode() returns a "unique hash value" for the object based on its memory address.
+
+The important point!!!!
+there is a contract between equals() and hashCode(), the developer need to maintain:
+(1): if one.equals(two), then one.hashCode() == two.hashCode()
+(2): if one.hashCode() == two.hashCode(), it is not necessary to maintain one.equals(two).
+When you want to override equals(), please definitely override hashCode() as well.
+
+Rehashing when it exceeds the load factor 0.75: number of <key, value> pair / number of buckets
